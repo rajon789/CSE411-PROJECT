3c2bce33c618066df337e271320031dbc57c145c
Rajon <rajon789@gmai.com>
2020-02-12 16:58:54 +0600

    2nd commit

--- MedicalAssurance/Registration/views.py
+++ MedicalAssurance/Registration/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 def home(request):
     user_name = 'zeesun and Rafa'
     return render(request,'home.html',{'name': user_name})
+
+def add(request):
+    val_1 = request.POST['number1']
+    val_2 = request.POST['number2']
+    try:
+        res = int(val_1) + int(val_2)
+    except:
+        return render(request,'result.html',{'result':'Not valid'})
+    return render(request,'result.html',{'result':res})
